289602a1780bc6a74aa6eb73ca2143c27590ef11
nanayawcodes <103840077+nanayawcodes@users.noreply.github.com>
2023-05-31 17:20:42 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./homepage/index.html"],
+  content: ["./homepage/index.html", "./workout-page.html"],
   theme: {
     extend: {},
     fontFamily: {
